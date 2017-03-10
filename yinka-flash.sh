0eb705a43ef5b887498ee188f6f8efa5351528bb
geekerlw <geekerlw@gmail.com>
2017-03-10 15:54:27 +0800

    initial flash repo

---
+++ yinka-flash.sh
@@ -0,0 +1,85 @@
+#!/bin/bash -e
+
+# set default port and device
+if [ ! $TTY ]; then
+	if [ -c "/dev/ttyUSB0" ]; then
+		TTY="/dev/ttyUSB0"
+	else
+		echo "no support tty device"
+		exit 1
+	fi
+fi
+if [ ! $DEVICE ]; then
+	if [ -b "/dev/sda" ]; then
+		DEVICE="/dev/sdc"
+	else
+		echo "no support block device"
+		exit 1
+	fi
+fi
+
+# prepare resource
+if [ -f update.tar.gz ]; then
+	tar -xvf update.tar.gz
+else
+	exit 1
+fi
+
+# flash uboot
+# make sure your board is in maskrom mode
+./tools/upgrade_tool db u-boot/prebuild/rk3288_boot.bin
+./tools/upgrade_tool wl 64 u-boot/u-boot-dtb.bin
+./tools/upgrade_tool wl 256 u-boot/u-boot.img
+./tools/upgrade_tool rd
+
+# wait seconds to restart board
+sleep 2
+
+# interrupt when start
+echo -e "\x03" > $TTY
+
+# write partition info to emmc
+echo -e "gpt write mmc 1 \$partitions" > $TTY
+
+# save gpt partition info on emmc
+echo -e "env save" > $TTY
+
+# change to ums mode
+echo -e "ums 0 mmc 1" > $TTY
+
+# wait seconds to restart board
+sleep 2
+
+# formatted partition
+mkfs.fat $DEVICE\1
+mkfs.ext4 $DEVICE\2 
+mkfs.ext4 $DEVICE\3
+
+# copy linux kernel to kernel partition
+mount $DEVICE\1 /mnt
+cp -av kernel/* /mnt
+umount /mnt
+
+# copy ubuntu file system to fs partition
+mount $DEVICE\2 /mnt
+cp -av ubuntu/* /mnt
+umount /mnt
+
+# copy ubuntu file system to backup partition
+mount $DEVICE\3 /mnt
+cp -av ubuntu/* /mnt
+umount /mnt
+
+# interrupt when ums stop
+echo -e "\x03" > $TTY
+
+# restart board
+echo -e "boot" > $TTY
+
+# remove no needed resource
+rm -rf kernel u-boot ubuntu
+
+# flash success
+echo -e "\033[1m\033[34m \n-------- Flash successful --------\n---- Auther: geekerlw@gmail.com ----\n \033[0m"
+
+exit 0
